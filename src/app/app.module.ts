7728244f68c6ad5c097f392e58500eefe996d23a
Rajat Raghuwanshi <raghuwanshi.rajat10@gmail.com>
2021-05-14 01:47:09 +0530

    Angular App Setup and basic MediaItem Component Generated

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './media-item/media-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MediaItemComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
