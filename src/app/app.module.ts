29901078b1de76433b680ffaa6a7348a56f04ae1
Rajat Raghuwanshi <raghuwanshi.rajat10@gmail.com>
2021-05-14 02:24:21 +0530

    Favorite User directive added to handle favorite icon of MediaItem Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MediaItemComponent
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective
   ],
   imports: [
     BrowserModule
