9fdc9376477afafb071d2f6efa36b7dd90a3c2dd
Rajat Raghuwanshi <raghuwanshi.rajat10@gmail.com>
2021-05-15 02:20:40 +0530

    Model-driven Angular Form Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
@@ -20,7 +20,7 @@ import { MediaItemFormComponent } from './media-item-form/media-item-form.compon
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
